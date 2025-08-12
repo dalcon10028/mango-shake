8b5018743023a835ed42e26b8d4d26d2d092d3de
corn-dunamu <corn@dunamu.com>
2025-08-12 16:05:49 +0900

    refactor: implement asynchronous data fetching with dataclass for order histories

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,4 +1,6 @@
 import asyncio
+import dataclasses
+
 import nest_asyncio
 import streamlit as st
 import pandas as pd
@@ -7,9 +9,32 @@ from service.order_history_service import get_order_histories
 
 # Allow nested event loops and run the async loader
 nest_asyncio.apply()
-data = asyncio.get_event_loop().run_until_complete(get_order_histories())
 
-df = pd.DataFrame.from_records([row.__dict__ for row in data]).drop(columns=['_sa_instance_state'], errors='ignore')
+@dataclasses.dataclass
+class FetchResult:
+    """
+    A dataclass to hold the result of the fetch operation.
+    """
+    order_histories: pd.DataFrame
+
+async def fetch_async_data():
+    """
+    Asynchronously fetches order histories and returns them as a FetchResult dataclass.
+    """
+
+    results = await asyncio.gather(*[
+        get_order_histories()
+    ])
+
+    results = [*map(lambda models: pd.DataFrame.from_records([row.__dict__ for row in models]).drop(columns=['_sa_instance_state'], errors='ignore'), results)]
+
+    return FetchResult(
+        order_histories=results[0]
+    )
+
+data: FetchResult = asyncio.get_event_loop().run_until_complete(fetch_async_data())
+
+df = data.order_histories
 
 st.write("Got lots of data? Great! Streamlit can show [dataframes](https://docs.streamlit.io/develop/api-reference/data) with hundred thousands of rows, images, sparklines – and even supports editing! ✍️")
 
