53e201827f901bf851f189a0a8900641221f791c
corn-dunamu <corn@dunamu.com>
2025-09-08 10:59:01 +0900

    feat: update product type to USDT-FUTURES in ticker API request

--- src/exchange/bitget/future/future_market_client.py
+++ src/exchange/bitget/future/future_market_client.py
@@ -23,7 +23,7 @@ class BitgetFutureMarketClient:
         Get ticker information for a specific symbol.
         {"code":"00000","msg":"success","requestTime":1695794095685,"data":[{"symbol":"ETHUSD_231229","lastPr":"1829.3","askPr":"1829.8","bidPr":"1829.3","bidSz":"0.054","askSz":"0.785","high24h":"0","low24h":"0","ts":"1695794098184","change24h":"0","baseVolume":"0","quoteVolume":"0","usdtVolume":"0","openUtc":"0","changeUtc24h":"0","indexPrice":"1822.15","fundingRate":"0","holdingAmount":"9488.49","deliveryStartTime":"1693538723186","deliveryTime":"1703836799000","deliveryStatus":"delivery_normal","open24h":"0","markPrice":"1829"}]}
         """
-        async with self._client.get("/api/v2/mix/market/ticker", params={ "productType": self._product_type, "symbol": symbol }) as resp:
+        async with self._client.get("/api/v2/mix/market/ticker", params={ "productType": "USDT-FUTURES", "symbol": symbol }) as resp:
             resp.raise_for_status()
             return await resp.json()
 
